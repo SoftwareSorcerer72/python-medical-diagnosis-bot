57b9e36527287850876bc8043951cdb1f8b12b9e
SoftwareSorcerer72 <mikewelborn72@gmail.com>
2024-06-12 11:46:53 -0400

    Completed lesson 9. Defined main function and some variables

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-print("Welcome Doctor!", "What would you like to do today?")
+welcome_prompt = "Welcome doctor, what would you like to do today?\n - To list all patients, press 1\n - To run a new diagnosis, press 2\n - To quit, press q\n"
 
-name = input("What is your name?\n")
-print(name)
+def list_patients():
+    print("Listing patients and diagnoses")
+
+def start_new_diagnosis():
+    print("Starting anew diagnosis")
+
+
+
+def main():
+    selection = input(welcome_prompt)
+    while selection != "q":
+        if selection =="1":
+            list_patients()
+            return main()
+        elif selection == "2":
+            start_new_diagnosis()
+            return main()        
+        elif selection == "q":
+            return
+       
+
+main()
